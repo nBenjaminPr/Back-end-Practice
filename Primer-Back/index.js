9750cdb1027a48264954251ffcc67c7b53c6f100
Nicolas <nicobenpe@gmail.com>
2023-04-27 20:11:07 -0300

    crando app con nodemon puerto 8000

--- Primer-Back/index.js
+++ Primer-Back/index.js
@@ -1,13 +1,58 @@
 // console.log("La suma de 2 mas 2 es " + (2+2));
 
-const http = require("http")
+// const http = require("http")
+const express = require("express")
+const app = express();
 
-const server = http.createServer((req, res) =>{
-    res.write("hola mundo");
-    res.statusCode=200;
-    res.end();
+app.get("/users",(req, res) => {
+    res.status(200).json({users: [{
+        name:"Nico"}, {name: "Loco"}, {name: "kales"}]})
 })
 
-server.listen(5000, () => {
-    console.log("Servidor escuchando...");
+app.get("/*",(req, res) => {
+    res.status(404).json({message: "hola error 404"})
 })
+
+app.listen(8000, ()=> {
+    console.log("escuchando puerto 8000...");
+})
+
+// const server = http.createServer((req, res) =>{
+//     switch (req.url) {
+//         case "/":
+//             res.setHeader("Content-Type", "text/html")
+//             res.write("hola mundo");
+//             res.statusCode=200;
+//             res.end();
+//             break;
+//         case "/404":    
+//         res.statusCode=404;
+//         res.setHeader("Content-Type", "text/html")
+//         res.write("Error vieja");
+//         res.end();
+//         break;
+    
+//         default:
+//             break;
+//         }
+// })
+
+// server.listen(5000, () => {
+//     console.log("Servidor escuchando...");
+// })
+
+
+// PETICIONES HTTP 
+// METHID: GET
+// HEADERS:
+// BODY:
+
+// RESPUESTA HTTP
+
+// STATUSCODE
+// 100 INFORMACION
+// 200 TODO OK
+// 300 REDIRECCIONES
+// 400 ERRORES EN EL CLEINTE
+// 500 ERRORES EN EL SERVIDOR
+// MESSAGE JSON HTML TXT
