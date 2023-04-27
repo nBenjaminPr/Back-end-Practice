1cd8005a2aad9fd4a4554d6b6cd5dffc387f213f
Nicolas <nicobenpe@gmail.com>
2023-04-27 19:30:34 -0300

    Servidor funcionando

--- Primer-Back/index.js
+++ Primer-Back/index.js
@@ -1,1 +1,13 @@
-console.log("La suma de 2 mas 2 es " + (2+2));
+// console.log("La suma de 2 mas 2 es " + (2+2));
+
+const http = require("http")
+
+const server = http.createServer((req, res) =>{
+    res.write("hola mundo");
+    res.statusCode=200;
+    res.end();
+})
+
+server.listen(5000, () => {
+    console.log("Servidor escuchando...");
+})
