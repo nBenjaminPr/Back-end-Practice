72583264e851a04c0d8f19dee5fde5cdef1cfd81
Nicolas <nicobenpe@gmail.com>
2023-04-29 21:05:06 -0300

    practice whit nodemon and postman

--- Tercer-Back/index.js
+++ Tercer-Back/index.js
@@ -5,9 +5,13 @@ const app = express();
 //res: La respuesta que le damos al front
 
 app.get("/users", (req, res)=>{
-    res.status(200).json({users: []})
+    res.status(200).json({message: "Todos los usuarios"})
+})
+
+app.post("/users", (req, res)=>{
+    res.status(200).json({message: "Se ha creado un usuarios"})
 })
 
 app.listen(4000, () => {
-    console.log("Server listening...");
+    console.log("Server listening on port 4000...");
 })
