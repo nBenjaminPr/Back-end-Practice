27c970c3568d8b8c6048e7dada6a26e15107ff3d
Nicolas <nicobenpe@gmail.com>
2023-05-04 20:35:28 -0300

    add utils

--- Cuarto-Back/controllers/usersControllers.js
+++ Cuarto-Back/controllers/usersControllers.js
@@ -1,8 +1,18 @@
+const customError = require("../utils/custom");
+
 const getUsers = (req, res) => {
     //TRAEMOS LOS USUARIOS DE LA DB
+    
+    try {
+        const {name, id} = req.body;
+    if (!id || !name)   throw new customError ("No hay id o no hay nombre", 400)
+    const product = {id, name}
+    res.status(201).json({message: "se creo el producto ", product })
+        
+    } catch (error) {
+        res.status(error.code || 500).json({message:error.message})
+    }
 
-    const users = ["diego", "Gaby"]
-    res.status(200).json({users})
 }
 
 
---
+++ Cuarto-Back/utils/custom.js
@@ -0,0 +1,8 @@
+class customError extends Error {
+    constructor(message, error) {
+        super(message);
+        this.error = error;
+}
+}
+
+module.exports = customError
