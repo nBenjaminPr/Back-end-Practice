0763da66c182e756ce1603104d341d38b679ba79
Nicolas <nicobenpe@gmail.com>
2023-05-07 14:05:02 -0300

    añadiendo propertieS

--- Cuarto-Back/models/User.js
+++ Cuarto-Back/models/User.js
@@ -1,5 +1,38 @@
 const {Schema, model} = require("moongose")
 
 const UseSchema = new Schema({
-    name:String
+    name:{
+        type:String,
+        uppercase: true,
+        minLength:[3, "Debe tener mas de 3 carecteres"],
+        maxLength:[40, "No debe tener mas de 40 caracteres"],
+    },
+    email:{
+        type:String,
+        required:[true, "El email es obligatorio"],
+        unique: [true, "Email ya registrado"],
+        trim: true,
+    },
+    lastame:{
+        type:String,
+        lowercase: true,
+        minLength:[3, "Debe tener mas de 3 carecteres"],
+        maxLength:[40, "No debe tener mas de 40 caracteres"],
+    },
+    age:{
+        type:Number,
+        select: false,
+        min: [0, "La edad no puede ser negativa"],
+        max: [150, "Que sos matusalen?"]
+    },
+    gender: {
+        type:String,
+        enum: ["M", "F"]
+    },
+
+    admin:{
+        type:Boolean,
+        default:false
+    },
+    hobbies:Array
 })
