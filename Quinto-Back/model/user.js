e4673d5198279281160725a982f886378ee758c1
Nicolas <nicobenpe@gmail.com>
2023-05-29 21:22:06 -0300

    añadiendo type y schema

--- Quinto-Back/model/user.js
+++ Quinto-Back/model/user.js
@@ -1,2 +1,13 @@
 const {Schema, model} = require ("momgoose")
 
+const UserShema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20,
+        match: /^[a-zA-Z]+$/,
+        default: "Usuario sin nomrbre",
+    }
+})
