55e8ce68263e4463fa44745dd2cfc15c73fa2c1c
Nicolas <nicobenpe@gmail.com>
2023-05-30 20:59:24 -0300

    aplicando csutom error

--- Quinto-Back/controllers/userControllers.js
+++ Quinto-Back/controllers/userControllers.js
@@ -1,8 +1,10 @@
-const User = require ("../model/Userss")
+const User = require ("../model/Userss");
+const CustomError = require("../utils/customError");
 
 const getUsers = async (req, res) => {
         try {
                 const users = await User.find();
+                throw new CustomError ("this is a text", 502)
                 res.status(200).json(users);
         } catch (error) {
                 res.status(error.code || 500).json({message: "i`m sorry, something is wrong"})
