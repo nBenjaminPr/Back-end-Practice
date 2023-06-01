521cc882f2bedcb3708856bfa2b59355d4aba3b5
Nicolas <nicobenpe@gmail.com>
2023-06-01 20:37:30 -0300

    añadiendo genders

--- Quinto-Back/controllers/userControllers.js
+++ Quinto-Back/controllers/userControllers.js
@@ -3,8 +3,14 @@ const CustomError = require("../utils/customError");
 
 const getUsers = async (req, res) => {
         try {
-                const users = await User.find();
-                throw new CustomError ("this is a text", 502)
+                const {gender} = req.query
+                let users;
+                if(!gender){
+                        users = await User.find()
+                }else{
+                        users = await User.find({gender})
+                }
+                // throw new CustomError ("this is a text", 502)
                 res.status(200).json(users);
         } catch (error) {
                 res.status(error.code || 500).json({message: "i`m sorry, something is wrong"})
